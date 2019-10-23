97f1d87b8f3be202d0083155b0920f0f789987bb
Ritika Mehta <mehtarit@umich.edu>
2019-10-22 23:42:42 -0500

    Update SwapNodes.cs

--- SwapNodes.cs
+++ SwapNodes.cs
@@ -8,20 +8,88 @@ class Solution {
     /*
      * Complete the swapNodes function below.
      */
+
+    public class TreeNode {
+       public  TreeNode RightChild {get; set;}
+       public  TreeNode LeftChild {get; set;}
+       public  int Depth {get; set;}
+       public  int Index {get; set;}
+       public  TreeNode Parent {get; set;}
+    }
+
+
+   static void InOrderTraversal(TreeNode root, List<int> result)
+   {
+       if(root == null) return;
+       if(root.LeftChild!=null){
+           InOrderTraversal(root.LeftChild, result);
+       }
+       result.Add(root.Index);
+       if(root.RightChild!=null){
+           InOrderTraversal(root.RightChild, result);
+       }
+   }
+
+   static void Print(List<int> list){
+       foreach(var number in list){
+           Console.Write(number + " ");
+       }
+       Console.WriteLine();
+   }
+   static TreeNode BuildTree(int[][] indexes, int i,  int depth, int index){
+        
+        if(i >= indexes.Length) return null;
+
+        var arr = indexes[i];
+        var result = new TreeNode {
+            Depth = depth,
+            Index = index,
+            Parent = null
+        };
+        
+        if(arr[0] == -1){
+            result.LeftChild = null;
+        }
+        else{
+            i++;
+            result.LeftChild = BuildTree(indexes, i, depth+1, arr[0]);
+        }
+        if(arr[1] == -1){
+            result.RightChild = null;
+        }
+        else{
+            i++;           
+            result.RightChild= BuildTree(indexes, i, depth+1, arr[1]);
+        }
+
+        if(result.LeftChild !=null) result.LeftChild.Parent = result;
+        if(result.RightChild !=null) result.RightChild.Parent = result;
+        return result;       
+    }
+
     static int[][] swapNodes(int[][] indexes, int[] queries) {
         int queryCount = queries.Length;
         int [][] result = new int[queryCount][];
 
-        //todo: BuildTree
+       TreeNode root = BuildTree(indexes, 0, 1, 1);
+       
 
-        for(int i = 0; i< queries.Length(); i++)
+        var listToPrint = new List<int>();
+       InOrderTraversal(root, listToPrint);
+       Print(listToPrint);
+
+        //want to see it has the correct array
+
+        for(int i = 0; i< queries.Length; i++)
         {
             int k = queries[i];
-
+            break;
             //todo: Breadth first traversal 
             //if depth is multiple of k, swap its children.
-
-            //todo: depth first traversal and print nodes in an array
+            
+            //todo: depth first traversal add to list
+           // var inOrderTraversal = InOrderTraversal(root);
+            //result[i] = inOrderTraversal;
             //assign array to result[i];
         }
 
@@ -51,7 +119,7 @@ class Solution {
 
         int[][] result = swapNodes(indexes, queries);
 
-        textWriter.WriteLine(String.Join("\n", result.Select(x => String.Join(" ", x))));
+       // textWriter.WriteLine(String.Join("\n", result.Select(x => String.Join(" ", x))));
 
         textWriter.Flush();
         textWriter.Close();
