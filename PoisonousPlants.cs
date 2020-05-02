415f8f8a0e1f41b1130403b9ec0de93132c7dfad
Ritika Mehta <mehtarit@umich.edu>
2020-05-02 11:03:13 -0500

    Update PoisonousPlants.cs
    Simple and efficient

--- PoisonousPlants.cs
+++ PoisonousPlants.cs
@@ -14,63 +14,36 @@ using System;
 
 class Solution {
 
-    // Complete the poisonousPlants function below.
-    static int poisonousPlants(int[] p) {
-        var stack1 = new Stack<int>();
-        var stack2 = new Stack<int>();
+class Pair {
+    public int val;
+    public int count;
+    public Pair(int val, int count) {
+        this.val = val;
+        this.count = count;
+    }
+}
 
-        int initialNumberOfPlants = p.Length;
-        int newNumberOfPlants = 0;
-        int dayCount = -1;
-
-        //initialize first stack
-        for(int i = 0; i < p.Length; i++){
-            stack1.Push(p[i]);
+// Complete the poisonousPlants function below.
+static int poisonousPlants(int[] p) {
+    var stack = new Stack<Pair>();
+    int maxDays = 0;
+    for (int i = p.Length - 1; i >= 0; i--) {
+        int temp = 0;
+        while (stack.Count!=0 && p[i] < stack.Peek().val) {
+            temp++;
+            Pair pair = stack.Pop();
+            temp = Math.Max(temp, pair.count);
         }
+        stack.Push(new Pair(p[i], temp));
+    }
 
-        while(initialNumberOfPlants != newNumberOfPlants){
-            dayCount++;
-            initialNumberOfPlants = stack1.Count;
-            while(stack1.Count !=0){
-                var current = stack1.Peek();
-                stack1.Pop();
-                if(stack1.Count == 0){
-                    stack2.Push(current);
-                    continue;
-                }
-                var left = stack1.Peek();
-                if(current <= left){
-                    stack2.Push(current);
-                }
-            }
-                newNumberOfPlants = stack2.Count;
-                if(newNumberOfPlants == initialNumberOfPlants){
-                    return dayCount;
-                }
-                dayCount++;
-
-                initialNumberOfPlants = newNumberOfPlants;
-                var first = stack2.Peek();
-                stack1.Push(first);
-
-                while(stack2.Count !=0){
-                    var current = stack2.Peek();
-                    stack2.Pop();
-                    if(stack2.Count == 0){
-                        continue;
-                    }
-                    var left = stack2.Peek();
-                    if(left <= current){
-                        stack1.Push(left);
-                    }
-                }              
-                newNumberOfPlants = stack1.Count;
-            }
-            
-             return dayCount;
-        
+    foreach(var s in stack){
+        if(s.count > maxDays){maxDays = s.count;
         }
+    }
 
+    return maxDays;
+}
 
     static void Main(string[] args) {
         TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
