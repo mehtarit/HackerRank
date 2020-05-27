50a8621cb66f949983738252d9ed2d1a855ee529
Ritika Mehta <mehtarit@umich.edu>
2020-05-27 11:28:56 -0500

    Update CrosswordPuzzle.cs
    Added some more code, still need to add logic to fill words though

--- CrosswordPuzzle.cs
+++ CrosswordPuzzle.cs
@@ -14,28 +14,60 @@ using System;
 
 class Solution {
 
+    static Dictionary<int, List<string>> GetWordLengthMap(string words){
+        var wordList = words.Split(';').ToList();
+        var result = new Dictionary<int, List<string>>();
+
+        foreach(var word in wordList){
+            if(!result.ContainsKey(word.Length)){
+                result.Add(word.Length,new List<string>(){word});
+                continue;
+            }
+            result[word.Length].Add(word);
+        }
+        return result;
+    }
+
     static string[] Fill(string[] crossword, Dictionary<Tuple<int, int>, char> map){
         for(int i=0; i< crossword.Length; i++){
             var row = crossword[i];
+            var stringBuilder= new StringBuilder(row);
             for(int j = 0; j < row.Length; j++){
                 var currentCell = Tuple.Create(i, j);
                 if(!map.ContainsKey(currentCell)) continue;
-                crossword[i][j] = map[currentCell];
-                //strings in c# are immutable so this will not work.
+                stringBuilder[j] = map[currentCell];
             }
+            crossword[i] = stringBuilder.ToString();
         }
-
         return crossword;
+    }
 
+    static void Print(Dictionary<int, List<string>> wordLengthMap){
+          foreach(var result in wordLengthMap){
+              Console.Write($"{result.Key}: ");
+              foreach(var word in result.Value){
+                  Console.Write($"{word} ");
+              }
+              Console.WriteLine();
+          }
+    }
+
+    static void FillMatrixMap(string[] crossword,
+    Dictionary<int, List<string>> wordLengthMap, 
+    Dictionary<Tuple<int, int>, char> matrixMap)
+    {
+        var tuple = Tuple.Create(1, 2);
+        matrixMap.Add(tuple, 'Z');
     }
 
     // Complete the crosswordPuzzle function below.
     static string[] crosswordPuzzle(string[] crossword, string words) {
-
-           var matrixMap = new Dictionary<Tuple<int, int>, char>();
-           var tuple = Tuple.Create(1, 2);
-           matrixMap.Add(tuple, 'Z');
-           return Fill(crossword,matrixMap);
+         
+          var wordLengthMap = GetWordLengthMap(words);          
+          Print(wordLengthMap);
+          var matrixMap = new Dictionary<Tuple<int, int>, char>();
+          FillMatrixMap(crossword, wordLengthMap, matrixMap);
+          return Fill(crossword,matrixMap);
     }
 
     static void Main(string[] args) {
